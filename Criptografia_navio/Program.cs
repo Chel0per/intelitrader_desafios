4bc4c481caf7ef8ddd5cafd40fb7accee91e1f50
Marcelo <marcelo.smarques7@gmail.com>
2024-08-03 11:21:26 -0300

    both challenges working

---
+++ Criptografia_navio/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+// See https://aka.ms/new-console-template for more information
+namespace Criptografia_navio
+{
+
+class Program
+{
+    static void Main()
+    {
+
+        string message = "10010110 11110111 01010110 00000001 00010111 00100110 01010111 00000001 00010111 01110110 01010111 00110110 11110111 11010111 01010111 00000011";
+
+        List<string> eightBitStrings = new List<string>(message.Split(" "));
+        List<string> newStrings = new List<string>();
+
+        foreach (string s in eightBitStrings)
+        {
+            
+            string firstPart = s[0..4];
+            string twoDigits = s[4..6];
+            string threeDigits;
+            string secondPart;
+
+            if(s[6] == '0')
+            {
+                threeDigits = twoDigits + '1';
+            }
+            else
+            {
+                threeDigits = twoDigits + '0';
+            }
+
+            if(s[7] == '0')
+            {
+                secondPart = threeDigits + '1';
+            }
+            else
+            {
+                secondPart = threeDigits + '0';
+            }
+
+            newStrings.Add(secondPart + firstPart);
+
+        }
+
+        List<char> asciiCharacters = new List<char>();
+
+        foreach (string bitString in newStrings)
+        {
+            int asciiValue = Convert.ToInt32(bitString, 2);
+            char character = (char)asciiValue;
+            asciiCharacters.Add(character);
+        }
+
+        string decryptedMessage = new string(asciiCharacters.ToArray());
+
+        Console.WriteLine(decryptedMessage);
+
+    }
+    
+}
+}
+
+
+
---
+++ Livro_de_ofertas/Program.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Livro_de_Ofertas 
+{
+class Program 
+{
+    static void Main()
+    {
+        string command = "12/n1,0,15.4,50/n2,0,15.5,50/n2,2,0,0/n2,0,15.4,10/n3,0,15.9,30/n3,1,0,20/n4,0,16.50,200/n5,0,17.00,100/n5,0,16.59,20/n6,2,0,0/n1,2,0,0/n2,1,15.6,0";
+        List<string> number_commands = new List<string>(command.Split("/n"));
+        string number = number_commands[0];
+        List<string> commands = number_commands.GetRange(1,int.Parse(number));
+        List<Offer> offers = new List<Offer>();
+        foreach (string line in commands)
+        {
+            List<string> numbers = new List<string>(line.Split(","));
+            Action action = new Action
+            (
+                int.Parse(numbers[0]),
+                int.Parse(numbers[1]),
+                double.Parse(numbers[2],CultureInfo.InvariantCulture),
+                int.Parse(numbers[3])
+            );
+
+            switch (action.Type)
+            {
+                case 0:
+                    if(action.Position == offers.Count + 1)
+                    {
+                        offers.Add(new Offer(action.Value,action.Quantity));
+                    }
+                    else if(action.Position <= offers.Count)
+                    {
+                        offers.Insert(action.Position - 1, new Offer(action.Value,action.Quantity));
+                    }
+                    else throw new Exception();
+                    break;
+                case 1:
+                    if(action.Value > 0 && action.Quantity > 0)
+                    {
+                        Offer offer = offers.ElementAt(action.Position - 1);
+                        offer.Quantity = action.Quantity;
+                        offer.Value = action.Value;
+                    }
+                    else if(action.Value > 0)
+                    {
+                        Offer offer = offers.ElementAt(action.Position - 1);
+                        offer.Value = action.Value;
+                    }
+                    else if(action.Quantity > 0)
+                    {
+                        Offer offer = offers.ElementAt(action.Position - 1);
+                        offer.Quantity = action.Quantity;                        
+                    }
+                    else throw new Exception();
+                    break;
+                case 2:
+                    offers.RemoveAt(action.Position - 1);
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        int i = 1;
+
+        foreach (Offer offer in offers)
+        {
+            string sValue = offer.Value.ToString(CultureInfo.InvariantCulture);
+            Console.WriteLine(i + "," + sValue + "," + offer.Quantity);
+            i++;
+        }
+    }
+}
+
+class Action
+{
+    private int position;
+    private int type;
+    private double value;
+    private int quantity;
+
+    public int Position
+    {
+        get { return position; }
+    }
+    public int Type
+    {
+        get { return type; }
+    }
+    public double Value
+    {
+        get { return value; }
+    }
+    public int Quantity
+    {
+        get { return quantity; }
+    }
+
+    public Action(int position, int type, double value, int quantity)
+    {
+        this.position = position;
+        this.type = type;
+        this.value = value;
+        this.quantity = quantity;
+    }
+}
+
+class Offer
+{
+    private double value;
+    private int quantity;
+
+    public double Value
+    {
+        get { return value; }
+        set { this.value = value;}
+    }
+    public int Quantity
+    {
+        get { return quantity;}
+        set { quantity = value;}
+    }
+
+    public Offer(double value, int quantity)
+    {
+        this.value = value;
+        this.quantity = quantity;
+    }
+}
+
+}
